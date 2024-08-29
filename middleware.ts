69677e9897c09bfcad3b9019dd9bb15c1a4bac17
adamrichardturner <hello@adamrichardturner.dev>
2024-08-29 04:11:33 +0100

    Middleware update

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,11 @@ import type { NextRequest } from "next/server"
 export function middleware(request: NextRequest) {
   const url = request.nextUrl
   const oobCode = url.searchParams.get("oobCode")
+  const redirect = url.searchParams.get("redirect")
 
-  // Check if the route is the email verification page and if the oobCode parameter is present
-  if (url.pathname === "/verify" && !oobCode) {
-    // Redirect to the homepage if the oobCode is missing
+  // Check if the route is the email verification page and if both oobCode and redirect parameters are present
+  if (url.pathname === "/verify" && (!oobCode || !redirect)) {
+    // Redirect to the homepage if either oobCode or redirect is missing
     return NextResponse.redirect(new URL("/", request.url))
   }
 
